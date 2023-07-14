ad315a08b9be457b381fe2d8faf843d4a2d6ab3d
joaopaulo <joaopaulo@joaopaulo-OptiPlex-780>
2023-07-14 04:38:33 -0300

    Meu Primeiro Commit;

---
+++ GraficosComJava/src/principal/Game.java
@@ -0,0 +1,42 @@
+package principal;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+
+	public static JFrame frame;
+	
+	private final int ALTURA = 120;
+	private final int LARGURA = 160;
+	private final int ESCALA = 4;
+	
+	public Game() {
+		this.setPreferredSize(new Dimension(LARGURA*ESCALA, ALTURA*ESCALA));
+		frame = new JFrame("Meu Primeiro Jogo em Java");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Game g = new Game();
+		
+
+	}
+
+	@Override
+	public void run() {
+		
+		
+	}
+
+}
